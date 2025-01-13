3b3391e5dc2ad236f5b75785606ae046ad19c7df
filipkk78 <filipkk78@gmail.com>
2025-01-13 23:23:40 +0100

    Add covered board display and hit display

--- src/index.js
+++ src/index.js
@@ -3,31 +3,6 @@ import Ship from "./ship";
 import Gameboard from "./gameboard";
 import Player from "./player";
 
-function loadBoard(player, board) {
-  while (board.firstChild) {
-    board.removeChild(board.lastChild);
-  }
-  for (let i = 0; i < 10; i++) {
-    for (let j = 0; j < 10; j++) {
-      const tile = document.createElement("div");
-      tile.className = "tile";
-      if (player.board.coords.get(`${i}, ${j}`).mark !== "X") {
-        const type = player.board.ships.at(
-          player.board.coords.get(`${i}, ${j}`).mark
-        ).type;
-        tile.textContent = type.charAt(0).toUpperCase();
-        tile.classList.add(`${type}`);
-      } else {
-        tile.textContent = player.board.coords.get(`${i}, ${j}`).mark;
-      }
-      if (player.board.coords.get(`${i}, ${j}`).hit) {
-        tile.classList.add("hit");
-      }
-      board.appendChild(tile);
-    }
-  }
-}
-
 const playerOne = new Player("human");
 playerOne.board.placeShip("carrier", 3, 4, "up");
 playerOne.board.placeShip("patrolboat", 1, 2, "down");
@@ -42,22 +17,72 @@ playerTwo.board.placeShip("submarine", 6, 8, "right");
 playerTwo.board.placeShip("patrolboat", 6, 6, "left");
 playerTwo.board.placeShip("battleship", 1, 2, "right");
 
+const playerDisplay = document.querySelector("#currPlayer");
+
+const game = {
+  loadBoard(player, board) {
+    while (board.firstChild) {
+      board.removeChild(board.lastChild);
+    }
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
+        const tile = document.createElement("div");
+        tile.className = "tile";
+        if (player.board.coords.get(`${i}, ${j}`).mark !== "X") {
+          const type = player.board.ships.at(
+            player.board.coords.get(`${i}, ${j}`).mark
+          ).type;
+          tile.textContent = type.charAt(0).toUpperCase();
+          tile.classList.add(`${type}`);
+        } else {
+          tile.textContent = player.board.coords.get(`${i}, ${j}`).mark;
+        }
+        if (player.board.coords.get(`${i}, ${j}`).hit) {
+          tile.classList.add("hit");
+        }
+        board.appendChild(tile);
+      }
+    }
+  },
+  attack(player, playerBoard, x, y) {
+    player.board.receiveAttack(x, y);
+    loadBoard(player, playerBoard);
+  },
+  loadCoveredBoard(board) {
+    while (board.firstChild) {
+      board.removeChild(board.lastChild);
+    }
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
+        const tile = document.createElement("div");
+        tile.className = "tile";
+        tile.textContent = "";
+        board.appendChild(tile);
+      }
+    }
+  },
+  playRound(player) {
+    if (player === playerOne) {
+      this.loadBoard(player, boardOne);
+      playerDisplay.textContent = "Current player: P1";
+      this.loadCoveredBoard(boardTwo);
+    } else {
+      this.loadBoard(player, boardTwo);
+      playerDisplay.textContent = "Current player: P2";
+      this.loadCoveredBoard(boardOne);
+    }
+  },
+  playGame() {},
+};
+
 const boardOne = document.querySelector("#boardOne");
-loadBoard(playerOne, boardOne);
-
 const boardTwo = document.querySelector("#boardTwo");
-loadBoard(playerTwo, boardTwo);
+game.loadBoard(playerOne, boardOne);
+game.loadBoard(playerTwo, boardTwo);
 
-function attack(player, playerBoard, x, y) {
-  player.board.receiveAttack(x, y);
-  loadBoard(player, playerBoard);
-}
-
-attack(playerOne, boardOne, 3, 4);
-attack(playerOne, boardOne, 3, 5);
-attack(playerOne, boardOne, 3, 6);
-attack(playerOne, boardOne, 3, 7);
-attack(playerOne, boardOne, 3, 8);
-attack(playerOne, boardOne, 1, 2);
-attack(playerOne, boardOne, 1, 1);
-console.log(playerOne.board);
+document.querySelector("footer").addEventListener("click", () => {
+  game.playRound(playerOne);
+});
+document.querySelector("header").addEventListener("click", () => {
+  game.playRound(playerTwo);
+});
